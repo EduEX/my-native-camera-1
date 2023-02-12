d167e9f63250cdbe5c6304222c98c83977f82add
Edu <eduardo.ex7@gmail.com>
2023-02-12 17:12:36 -0300

    Alt. 2

--- App.js
+++ App.js
@@ -42,8 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: 'center',
-    backgroundColor: "orange"
+    justifyContent: 'center'
   },
   camera:{
     width: "100%",
